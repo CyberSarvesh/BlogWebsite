f8540fe00ffcc1c39e272cf2959508f4c67819cb
Sarvesh Mohite <142711109+CyberSarvesh@users.noreply.github.com>
2024-10-17 18:08:15 +0530

    Revert "Sarvesh branch"

--- index.js
+++ index.js
@@ -2,24 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import connectdb from './connectdb.js';
-import userRoutes from './routes/userRoutes.js';
-import blogRoutes from './routes/blogRoutes.js';
+
+
 dotenv.config();
 
 const app = express();
 
-connectdb();
-
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8000;
-
-//api redirecting to /routes directory for each type of request
-app.use("/api/user",userRoutes);
-app.use("/api/blogs",blogRoutes);
-
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
